d8e83a66dc31d0b149a3b15d9b149790e5d504be
Miika Nissi <miika@miikanissi.com>
2024-12-21 21:59:38 -0500

    ci: update versions of linters and formatters

---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+
+const config = {
+  bracketSpacing: false,
+  printWidth: 88,
+  proseWrap: "always",
+  semi: true,
+  trailingComma: "es5",
+};
+
+module.exports = config;
